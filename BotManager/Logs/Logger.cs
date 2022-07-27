b2fdca31d0564f9f7662a9362fd619ab08b19bd9
Vaness000 <rayzhaev99@mail.ru>
2022-07-27 11:27:29 +0300

    added log feature

---
+++ BotManager/Logs/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace BotManager.Logs
+{
+    public class Logger
+    {
+        private static string path = Path.Combine(Environment.CurrentDirectory, "log.txt");
+
+        public static void Log(LogType type, string logMessage)
+        {
+            File.AppendAllText(path, $"{DateTime.Now} {type}: {logMessage}{Environment.NewLine}");
+        }
+    }
+}
